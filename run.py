7e3552c330bb43d417dbc8d6824a1a9f7c81772c
YIER34 <2536650222@qq.com>
2020-09-09 17:46:14 +0800

    changed on 20200909

--- run.py
+++ run.py
@@ -4,12 +4,12 @@ from utils.HTMLTestRunner import HTMLTestRunner
 
 
 # 收集测试用例
-t = unittest.defaultTestLoader.discover("F:/newpro/testpro\wawhtest\case", "test_*.py")
+t = unittest.defaultTestLoader.discover("F:/newpro/testpro/wawh_test/case", "test_*.py")
 
 # 运行所有的case并生成测试报告
-title = "测试报告"
+title = "==测试报告=="
 descr = "这是第一次测试报告"
-file_path = "F:/newpro/testpro/wawhtest/report/report.html"
+file_path = "F:/newpro/testpro/wawh_test/report/report.html"
 
 with open(file_path, "wb") as f:
     runner = HTMLTestRunner(stream=f, title=title, description=descr)
